973f6f722a56cd7840cfbe2896c3c2ef552ecd0a
chawki505 <chouibc@gmail.com>
2016-03-22 19:55:44 +0100

    none

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,9 @@
 
 public class Main {
 
-
     public static void main(String[] args) {
 
         BanqueInteractive BanqueChawkiEtIshak = new BanqueInteractive();
-
-        //pour avoir aumoin 2 client au demarage du programme
-        BanqueChawkiEtIshak.ajouterClient("chawki");
-        BanqueChawkiEtIshak.ajouterClient("ishak");
-
         BanqueChawkiEtIshak.interaction();
 
     }
